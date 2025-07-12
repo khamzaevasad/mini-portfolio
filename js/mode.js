190a7b45e699b21f997f82260653271a890c657f
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-12 22:24:45 +0900

    Add dark mode toggle functionality and corresponding styles
    - Introduced a new JavaScript file (mode.js) to handle dark mode toggling.
    - Implemented event listeners for dark and light mode buttons.
    - Utilized localStorage to remember the user's mode preference.
    - Added CSS classes to switch between dark and light modes.
    - Included a new font file (dm-sans-v16-latin-regular.woff2) for improved typography.

---
+++ js/mode.js
@@ -0,0 +1,26 @@
+const body = document.querySelector("body");
+const darkBtn = document.getElementById("dark-btn");
+const lightBtn = document.getElementById("light-btn");
+
+const modeLocal = localStorage.getItem("mode");
+if (modeLocal) {
+  body.classList.add("dark__mode");
+  darkBtn.classList.toggle("hidden");
+  lightBtn.classList.toggle("hidden");
+}
+
+const toggleMode = () => {
+  darkBtn.classList.toggle("hidden");
+  lightBtn.classList.toggle("hidden");
+  body.classList.toggle("dark__mode");
+};
+
+darkBtn.addEventListener("click", () => {
+  toggleMode();
+  localStorage.setItem("mode", "dak-mode");
+});
+
+lightBtn.addEventListener("click", () => {
+  toggleMode();
+  localStorage.setItem("mode", "");
+});
